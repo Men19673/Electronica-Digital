7ab1879b6ad391b83ae964e6c5657d3b59a54f61
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-07-31 22:56:57 -0600

    Update tabla01POS.v
    Funciona

--- Lab 03/Tabla01_SOP/tabla01POS.v
+++ Lab 03/Tabla01_SOP/tabla01POS.v
@@ -1,8 +1,8 @@
-module tabla01();
+module tabla01POS();
 
 //POS tabla01
 
-  wire outAN,outBN,outCN,out01,out02,out03, outres;
+  wire outAN,outBN,outCN,out01,out02,out03,outres;
 
   reg inA,inB,inC;
 
@@ -10,30 +10,31 @@ module tabla01();
   not U2 (outBN, inB);
   not U3 (outCN, inC);
 
-  or U4 (outO1,inA,inB,outCN);
+  or U4 (out01,inA,inB,outCN);
   or U5 (out02,inA,outBN,outCN);
   or U6 (out03,outAN,outBN,inC);
   and U4(outres,out01,out02,out03);
 
   initial begin
+
     $display("A B C | Y");
     $display("---------");
-    $monitor( "%b %b b% | %b", inA, inB, inC, outres);
-    A=0; B=0; C=0;
-    #1 A=0, B=0, C=1;
-    #1 A=0, B=1, C=0;
-    #1 A=0, B=1, C=1;
-    #1 A=1, B=0, C=0;
-    #1 A=1, B=0, C=1;
-    #1 A=1, B=1, C=0;
-    #1 A=1, B=1, C=1;
+    $monitor("%b %b %b %b", inA, inB, inC, outres);
+    #1 inA=0; inB=0; inC=0;
+    #1 inA=0; inB=0; inC=1;
+    #1 inA=0; inB=1; inC=0;
+    #1 inA=0; inB=1; inC=1;
+    #1 inA=1; inB=0; inC=0;
+    #1 inA=1; inB=0; inC=1;
+    #1 inA=1; inB=1; inC=0;
+    #1 inA=1; inB=1; inC=1;
     #1 $finish;
   end
 
   initial
     begin
-      $dumpfile("tabla01POS_tb.v");
-      $dumpvars(0, tabla01POS);
+      $dumpfile("tabla01POS_tb.vcd");
+      $dumpvars(0,tabla01POS);
     end
 
 endmodule
