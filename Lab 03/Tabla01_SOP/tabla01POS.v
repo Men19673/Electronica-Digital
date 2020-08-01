e2fee4befa6b93a3d3ce76c0a61e9224509decd0
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-07-31 22:28:52 -0600

    correcion de punto coma

--- Lab 03/Tabla01_SOP/tabla01POS.v
+++ Lab 03/Tabla01_SOP/tabla01POS.v
@@ -10,23 +10,23 @@ module tabla01();
   not U2 (outBN, inB);
   not U3 (outCN, inC);
 
-  or U4 (outO1,inA,inB,outCN)
-  or U5 (out02,inA,outBN,outCN)
-  or U6 (out03,outAN,outBN,inC)
-  and U4(outres,out01,out02,out03)
+  or U4 (outO1,inA,inB,outCN);
+  or U5 (out02,inA,outBN,outCN);
+  or U6 (out03,outAN,outBN,inC);
+  and U4(outres,out01,out02,out03);
 
   initial begin
-    $display("A B C | Y")
-    $display("---------")
-    $monitor( "%b %b b% | %b", inA, inB, inC, outres)
+    $display("A B C | Y");
+    $display("---------");
+    $monitor( "%b %b b% | %b", inA, inB, inC, outres);
     A=0; B=0; C=0;
-    #1 A=0; B=0; C=1;
-    #1 A=0; B=1; C=0;
-    #1 A=0; B=1; C=1;
-    #1 A=1; B=0; C=0;
-    #1 A=1; B=0; C=1;
-    #1 A=1; B=1; C=0;
-    #1 A=1; B=1; C=1;
+    #1 A=0, B=0, C=1;
+    #1 A=0, B=1, C=0;
+    #1 A=0, B=1, C=1;
+    #1 A=1, B=0, C=0;
+    #1 A=1, B=0, C=1;
+    #1 A=1, B=1, C=0;
+    #1 A=1, B=1, C=1;
     #1 $finish;
   end
 
