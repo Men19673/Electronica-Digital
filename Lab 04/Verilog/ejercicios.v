f423d8bf71ee53896df97493b42815b0c2104889
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-08-07 23:45:21 -0600

    Funcion hasta el eje3
    Faltaba & en el Ej1_3OP

--- Lab 04/Verilog/ejercicios.v
+++ Lab 04/Verilog/ejercicios.v
@@ -23,19 +23,12 @@ module E1_2GL(input wire A, B, C, output wire Y2);
   wire outAN, outBN, outCN, out01, out02, out03, out04;
 
 //not's
-  not U1 (outAN,A);
-  not U2 (outBN,B);
-  not U3 (outCN,C);
-
-//compuertas
-  and U4 (out01, outAN, outBN);
-  and U5 (out02, A, outBN);
-  or U7 (Y2, out01, out02, B);
+  not U2 (Y2,B);
 
 endmodule
 
-//module E1_3OP(input wire A, B, C, D, output wire Y3);
-  //ejercicio 1.3 con operadores
-  //assign Y3 = (~A & ~B & ~C & ~D) | (~A & ~B & C & D) | (~A & B & ~C & D) | (~A & B & C ~D) | (A & B & ~C & ~D) | (A & B & C & D) | (A & ~B & ~C & D) | (A & ~B & C & ~D);
+module E1_3OP(input wire A, B, C, D, output wire Y3);
+//ejercicio 1.3 con operadores
+  assign Y3 = (~A & ~B & ~C & ~D) | (~A & ~B & C & D) | (~A & B & ~C & D) | (~A & B & C & ~D) | (A & B & ~C & ~D) | (A & B & C & D) | (A & ~B & ~C & D) | (A & ~B & C & ~D);
 
-//endmodule
+endmodule
--- Lab 04/Verilog/ejercicios_tb.v
+++ Lab 04/Verilog/ejercicios_tb.v
@@ -1,11 +1,16 @@
 module testbench();
 // registrar entradas
   reg inA1, inB1, inC1, inD1;
+  reg inA2, inB2, inC2, inD2;
+  reg inA3, inB3, inC3, inD3;
   wire Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8;
 
   E1_1GL E1(inA1, inB1, inC1, Y1);
-  initial begin                                                                   //iniciamos el módulo Ej1.1
-
+  E1_2GL E2(inA2, inB2, inC2, Y2);
+  E1_3OP E3(inA3, inB3, inC3, inD3, Y3);
+  initial begin
+    $display(" ");                                                               //iniciamos el módulo Ej1.1
+    $display("EJERCICIO 1.1");
     $display("A B C | Y");
     $display("----------");
     $monitor("%b %b %b  %b",inA1,inB1,inC1,Y1);
@@ -19,9 +24,48 @@ module testbench();
     #1 inA1=1; inB1=1; inC1=1;
     end
 
+  initial begin                                                                 //iniciamos el módulo Ej1.1
+    #9
+    $display("");
+    $display("EJERCICIO 1.2");
+    $display("A B C | Y");
+    $display("----------");
+    $monitor("%b %b %b  %b",inA2,inB2,inC2,Y2);
+       inA2=0; inB2=0; inC2=0;
+    #1 inA2=0; inB2=0; inC2=1;
+    #1 inA2=0; inB2=1; inC2=0;
+    #1 inA2=0; inB2=1; inC2=1;
+    #1 inA2=1; inB2=0; inC2=0;
+    #1 inA2=1; inB2=0; inC2=1;
+    #1 inA2=1; inB2=1; inC2=0;
+    #1 inA2=1; inB2=1; inC2=1;
+    end
 
-  initial
-    #10 $finish;
+  initial begin
+    #18
+      $display(" ");                                                               //iniciamos el módulo Ej1.1
+      $display("EJERCICIO 1.3");
+      $display("A B C D | Y");
+      $display("----------");
+      $monitor("%b %b %b %b  %b",inA3,inB3,inC3,inD3,Y3);
+        inA3=0; inB3=0; inC3=0; inD3=0;
+      #1 inA3=0; inB3=0; inC3=0; inD3=1;
+      #1 inA3=0; inB3=0; inC3=1; inD3=0;
+      #1 inA3=0; inB3=0; inC3=1; inD3=1;
+      #1 inA3=0; inB3=1; inC3=0; inD3=0;
+      #1 inA3=0; inB3=1; inC3=0; inD3=1;
+      #1 inA3=0; inB3=1; inC3=1; inD3=0;
+      #1 inA3=0; inB3=1; inC3=1; inD3=1;
+      #1 inA3=1; inB3=0; inC3=0; inD3=0;
+      #1 inA3=1; inB3=0; inC3=0; inD3=1;
+      #1 inA3=1; inB3=0; inC3=1; inD3=0;
+      #1 inA3=1; inB3=0; inC3=1; inD3=1;
+      #1 inA3=1; inB3=1; inC3=0; inD3=0;
+      #1 inA3=1; inB3=1; inC3=0; inD3=1;
+      #1 inA3=1; inB3=1; inC3=1; inD3=0;
+      #1 inA3=1; inB3=1; inC3=1; inD3=1;
+    end
+
 
   initial begin
 
