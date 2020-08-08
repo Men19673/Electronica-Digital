4a982ca89d0444bfab8ee0f6bb1076c4f74ea28e
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-08-07 18:53:36 -0600

    Create ejercicios.v
    Primeros ejercicios

---
+++ Lab 04/Verilog/ejercicios.v
@@ -0,0 +1,34 @@
+module E1.1GL(input wire A, B, output wire Y);
+//ejercicio 1.1 con GateLevel Modeling
+
+  wire outAN, outBN, outCN, out01, out02, out03, out04;
+
+//not's
+  not U1 (outAN,A);
+  not U2 (outBN,B);
+  not U3 (outCN,C);
+
+//compuertas
+
+  and U4 (out01, outAN, outCN);
+  and U5 (out02, A, C);
+  and U6 (out03, A, outBN);
+  or U7 (Y, out01, out02, out03);
+endmodule
+
+module E1.2GL(input wire A, B, output wire Y);
+//ejercicio 1.2 con GateLevel Modeling
+
+  wire outAN, outBN, outCN, out01, out02, out03, out04;
+
+//not's
+  not U1 (outAN,A);
+  not U2 (outBN,B);
+  not U3 (outCN,C);
+
+//compuertas
+
+  and U4 (out01, outAN, outBN);
+  and U5 (out02, A, outBN);
+  or U7 (Y, out01, out02, B);
+endmodule
