e7086f18845de0af5bdc3e3a33ea402664b41c60
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-08-15 22:38:53 -0600

    Funciona Tabla 1

--- Lab 05/Verilog/Ejercicio4.v
+++ Lab 05/Verilog/Ejercicio4.v
@@ -17,11 +17,49 @@ module mux4_1(input wire inA, inB, inC, inD, input wire[1:0] inS, output wire Y)
 
 endmodule
 
+
 module mux8_1(input wire inA, inB, inC, inD, inE, inF, inG, inH, input wire[2:0] inS, output wire Y);
 //Creacion de un mux 8 a 1 basado
   wire Y1, Y2;
   mux4_1 U1(inA, inB, inC, inD, inS[1:0], Y1);
   mux4_1 U2(inE, inF, inG, inH, inS[1:0], Y2);
   mux2_1 U3(Y1, Y2, inS[2], Y);
+
+endmodule
+
+
+
+module tabla1mux8(input wire[2:0] inS, output wire Y);
+//modulo para mux 8 tabla 1
+
+//registrar cables
+  wire inA1, inB1, inC1, inD1, inE1, inF1, inG1, inH1;
+
+//asignar valores
+  assign inA1=0; assign inB1=1; assign inC1=1; assign inD1=0;
+  assign inE1=1; assign inF1=0; assign inG1=0; assign inH1=1;
+
+  mux8_1 U1(inA1, inB1, inC1, inD1, inE1, inF1, inG1, inH1, inS, Y);
+endmodule
+
+
+module tabla1mux4(input wire inC, input wire[1:0] inS, output wire Y);
+  //aplicando la primera tabla con un mux4a1 y compuertas
+  wire NC;
+
+  not N1(NC,inC);
+// la compuerta not nos sirve para trabajar unicamente con una entrada C y el mux 4
+  mux4_1 U1(inC,NC,NC,inC,inS, Y);
+endmodule
+
+module tabla1mux2(input wire inS, inB, inC, output wire Y);
+
+  wire Y1, Y2;
+
+  assign Y1= inB ^ inC ;
+  assign Y2= inB ~^ inC;
+
+  mux2_1 U1(Y1, Y2, inS, Y);
+
 
 endmodule
--- Lab 05/Verilog/Ejercicio4_tb.v
+++ Lab 05/Verilog/Ejercicio4_tb.v
@@ -4,33 +4,76 @@
 
 module testbench();
 // registrar entradas
-  reg inA1, inB1, inC1, inD1, inE1, inF1, inG1, inH1;
-  reg[2:0] inS;
-  wire Y;
-  mux8_1 U1(inA1, inB1, inC1, inD1, inE1, inF1, inG1, inH1, inS, Y);
+
+  reg inA2, inB2, inC2;
+  reg inA3, inB3, inC3;
+
+
+  reg[2:0] inS1, inS4;
+  reg[1:0] inS2, inS5;
+  reg inS3, inS6;
+  wire Y1, Y2, Y3, Y4, Y5, Y6;
+
+  //Llamando a los modulos
+    tabla1mux8 U1(inS1, Y1);
+    tabla1mux4 U2(inC2, inS2, Y2);
+    tabla1mux2 U3(inS3, inB3, inC3 ,Y3);
+
+
   initial begin //iniciamos el mux8_1
     $display(" ");
-    $display("Mux8_1");
-    $display("A B C | Y");
+    $display("Mux8 a 1");
+    $display("ABC | Y");
     $display("----------");
-    $monitor("%b  %b",inS ,Y);
-       inA1=0; inB1=1; inC1=1; inD1=0;
-       inE1=1; inF1=0; inG1=0; inH1=1;
-         inS=000;
-      #1 inS=001;
-      #1 inS=010;
-      #1 inS=011;
-      #1 inS=100;
-      #1 inS=101;
-      #1 inS=110;
-      #1 inS=111;
+    $monitor("%b | %b",inS1 ,Y1);
+
+         inS1=000;
+      #1 inS1=001;
+      #1 inS1=010;
+      #1 inS1=011;
+      #1 inS1=100;
+      #1 inS1=101;
+      #1 inS1=110;
+      #1 inS1=111;
       end
 
+    initial begin //iniciamos el mux8_1
+    #8
+        $display(" ");
+        $display("Mux4 a 1");
+        $display("ABC | Y");
+        $display("----------");
+        $monitor("%b%b | %b",inS2, inC2 ,Y2);
+             inS2=00; inC2=0;
+          #1 inS2=00; inC2=1;
+          #1 inS2=01; inC2=0;
+          #1 inS2=01; inC2=1;
+          #1 inS2=10; inC2=0;
+          #1 inS2=10; inC2=1;
+          #1 inS2=11; inC2=0;
+          #1 inS2=11; inC2=1;
+          end
 
+      initial begin //iniciamos el mux8_1
+      #16
+          $display(" ");
+          $display("Mux4 a 1");
+          $display("ABC | Y");
+          $display("----------");
+          $monitor("%b%b%b | %b",inS3, inB3, inC3 ,Y3);
+               inS3=0; inB3=0; inC3=0;
+            #1 inS3=0; inB3=0; inC3=1;
+            #1 inS3=0; inB3=1; inC3=0;
+            #1 inS3=0; inB3=1; inC3=1;
+            #1 inS3=1; inB3=0; inC3=0;
+            #1 inS3=1; inB3=0; inC3=1;
+            #1 inS3=1; inB3=1; inC3=0;
+            #1 inS3=1; inB3=1; inC3=1;
+            end
 
     initial
 
-    #10 $finish;
+    #25 $finish;
 
   initial begin
 
