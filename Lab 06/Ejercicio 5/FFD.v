ba96937778492ab512e46b6d771ec0923582c500
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-09-04 23:55:59 -0600

    Flip Flop Funcional

--- Lab 06/Ejercicio 5/FFD.v
+++ Lab 06/Ejercicio 5/FFD.v
@@ -2,11 +2,23 @@
 //Ejercicio 5
 
 //FlipFlop 4 Bits
-module ffD(input wire clk,
-           input wire[3:0] D,
+module ffD(input clk, reset,
+           input[3:0] D,
            output reg[3:0] Q);
 
-    always @(posedge clk, reset);
-      if(reset) Q<=4'b0
+    
+
+    always @(posedge clk, posedge reset) begin
+      //Hacemos que el reset haga Q = 0
+      if(reset) begin
+        Q <= 4'b0;
+        end
+      //Hacemos que Q se vuelva D
+      else
+        Q <= D;
+        end
+
+
+
 
 endmodule
--- Lab 06/Ejercicio 5/FFD_tb.v
+++ Lab 06/Ejercicio 5/FFD_tb.v
@@ -1,2 +1,44 @@
 //Diego A. Méndez
 //TestBench Ejercicio5
+
+module testbench();
+
+  reg clk, reset;
+  reg[3:0] D;
+  wire[3:0] Q;
+
+    always
+      begin
+      clk <= 1; #1 clk<=0; #1;
+      end
+
+    ffD U1(clk, reset, D, Q);
+
+
+     initial begin //iniciamos el mux8_1
+      $display(" ");
+      $display("  ");
+      $display(" ");
+      $display("CLK | RST | D | Q");
+      $display("----------");
+      $monitor("%b | %b | %b | %b",clk ,reset, D, Q);
+           D<=000;
+        #1 D<=001;
+        #1 D<=010;
+        #1 D<=011;
+        #1 D<=100;
+        #1 D<=101;
+        #1 D<=110;
+        #1 D<=111;
+
+        end
+
+  initial
+  #9 $finish;
+
+  initial begin
+    $dumpfile("FFD_tb.vcd");
+    $dumpvars(0, testbench);
+  end
+
+endmodule
