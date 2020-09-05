71150f674b1aa7d2e10ed88e1e67ba9aed5a376f
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-09-05 03:17:19 -0600

    comentarios por pasos
    este se basa en el ejercicio 1 que al mismo tiempo se basa en el flip flop del ejercicio 5

--- Lab 06/Ejercicio 1/FSM.v
+++ Lab 06/Ejercicio 1/FSM.v
@@ -18,16 +18,16 @@ endmodule
 module FSM(input wire A, B, clk, reset, output Y );
   wire Sf1, Sf0;
   wire S1, S0;
-
+//negamos los cables de estado
   not(nS1, S1);
   not(nS0, S0);
 
-
+//colocamos las ecuaciones del estado futuro
 
   assign Sf1= S0 & B + S1 & A & B;
   assign Sf0= nS1 & nS0 & A;
   assign Y= S1 & A & B;
-
+// conectamos los flip flops
   ffD U1(clk, reset, Sf1, S1);
   ffD U2(clk, reset, Sf0, S0);
 
--- Lab 06/Ejercicio 3/FSM.v
+++ Lab 06/Ejercicio 3/FSM.v
@@ -1,7 +1,7 @@
 //Diego A. Méndez
-//Ejercicio 1
+//Ejercicio 3
 
-//FlipFlop del ejercicio 5
+//FlipFlop del ejercicio 1
 module ffD(input clk, reset, input D, output reg Q);
 
     always @(posedge clk, posedge reset) begin
@@ -14,20 +14,20 @@ module ffD(input clk, reset, input D, output reg Q);
         Q <= D;
       end
 endmodule
-//Modulo para FSM
+//Modulo basado en el ejercicio 1
 module FSM(input wire A, clk, reset, output Y );
   wire Sf2, Sf1, Sf0;
   wire S2, S1, S0;
-
+//negacion de las compuertas
   not(nS2, S2);
   not(nS1, S1);
   not(nS0, S0);
   not(nA, A);
-
+//assignasion de las ecuaciones de estado proximo
   assign Sf2 = S2 & S0 + A & S1 & nS0 + nA & nS1 & nS0;
   assign Sf1 = S1 & nS0 + nA & S2 & S0 + A & nS2 & S0;
   assign Sf0 = A & S2 & S1 + nA & nS2 & S1 + nA & S2 & nS1 + A & nS2 & nS1;
-
+//conexion de los flip flops
   ffD U1(clk, reset, Sf1, S1);
   ffD U2(clk, reset, Sf0, S0);
   ffD U3(clk, reset, Sf2, S2);
