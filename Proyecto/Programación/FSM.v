932a381b5332cd96d99ce052d4cfbab1670ac235
Men19673 <68131298+Men19673@users.noreply.github.com>
2020-10-09 23:13:26 -0600

    Posible funcionamiento
    El codigo solo se copio de Logic y se arreglo para formato verilog, aun no se prueba

--- Proyecto/Programación/FSM.v
+++ Proyecto/Programación/FSM.v
@@ -17,7 +17,7 @@ module ffD(input clk, reset, input D, output reg Q);
 endmodule
 
 //Modulo para FSM
-module FSM Principal(input wire[1:0] ME, SSP, PF, input wire EL, LP,  clk, reset, output Y );
+module FSM Principal(input wire[1:0]  ME, SSP, PF, input wire EL, LP,  clk, reset, output wire[1:0] LCD, LCD2, Mo, output wire MP,LED, SN);
   wire SF3, SF2 ,SF1, SF0;
   wire SA3 ,SA2, SA1, SA0;
 
@@ -40,24 +40,33 @@ assign SF1 = ~SA3 & SA1 & SA0  + SA3 & ~SA2 & SA1 & ~SA0  + ~SA3 & ~SA0 & ~ME[1]
 assign SF0 = ~SA3 & SA0 & LP + ~SA3 & LP & ~ME[0] + SA3 & SA2 & SA1 & ~ME[0] + ~SA3 & ~SA0 & ME[1] & ~ME[0]  + SA3 & ~SA1 & SA0 & ~SSP[1]  + SA3 & ~SA2 & SA0 & SSP[0] + SA3 & ~SA1 & SA0 & SSP[0]
 + SA3 & ~SA2 & SA1 & SA0 & SSP[1]  + ~SA3 & SA1 & ~SA0 & ~PF[0] & EL & ME[0] + ~SA3 & ~SA2 & ~SA0 & PF[1] & ~PF[0] & EL & ~ME[0]  + ~SA3 & SA2 & ~SA0 & ~PF[1] & ~PF[0] & EL & ~ME[0];
 
-assign LCD1 = SA3 & ~SA2 & ~SA1  + ~SA3 & SA2 & SA0 + ~SA3 & SA2 & ~EL & ~ME[1] & ~ME[0] + ~SA3 & SA2 & LP & ~ME[1] & ~ME[0] + ~SA3 & ~SA0 & PF[1] & EL & ~LP & ~ME[1] & ~ME[0] + SA2 & ~SA1 & SA0;
+assign LCD[1] = SA3 & ~SA2 & ~SA1  + ~SA3 & SA2 & SA0 + ~SA3 & SA2 & ~EL & ~ME[1] & ~ME[0] + ~SA3 & SA2 & LP & ~ME[1] & ~ME[0] + ~SA3 & ~SA0 & PF[1] & EL & ~LP & ~ME[1] & ~ME[0] + SA2 & ~SA1 & SA0;
 
-assign LCD0 = ~SA3 & SA1 & SA0  + SA3 & ~SA2 & ~SA0 + SA3 & ~SA1 & ~SA0 + ~SA3 & SA1 & ~EL & ~ME[1] & ~ME[0] + ~SA3 & SA1 & LP & ~ME[1] & ~ME[0]
+assign LCD[0] = ~SA3 & SA1 & SA0  + SA3 & ~SA2 & ~SA0 + SA3 & ~SA1 & ~SA0 + ~SA3 & SA1 & ~EL & ~ME[1] & ~ME[0] + ~SA3 & SA1 & LP & ~ME[1] & ~ME[0]
 + ~SA3 & ~SA0 & PF[0] & EL & ~LP & ~ME[1] & ~ME[0] ;
 
-assign LCD2_1 = SA2 & SA1 & ~SA0 & ME[1] & ~ME[0] + SA3 & ~SA2 & SA1 & SA0 & SSP[1]  + SA3 & ~SA2 & SA1 & SA0 & SSP[0] + ~SA3 & SA2 & SA1 & ~SA0 & ~PF[0] & EL & ~LP & ~ME[0] + SA3' SA2 SA0' PF1' EL LP' ME1' ME0'  + SA3' SA1 SA0' PF1' PF0' EL LP' ME1' ME0' 
- + SA3 SA2 SA1 ME1 ME0'  + SA3 SA2 SA1' SA0 SSP1'  + SA3 SA2 SA1' SSP1 SSP0 + SA3 SA2 SA1' SA0' SSP0';
+assign LCD2[1] = SA2 & SA1 & ~SA0 & ME[1] & ~ME[0] + SA3 & ~SA2 & SA1 & SA0 & SSP[1]  + SA3 & ~SA2 & SA1 & SA0 & SSP[0] + ~SA3 & SA2 & SA1 & ~SA0 & ~PF[0] & EL & ~LP & ~ME[0]
+ + ~SA3 & SA2 & ~SA0 & ~PF[1] & EL & ~LP & ~ME[1] & ~ME[0] + ~SA3 & SA1 & ~SA0 & ~PF[1] & ~PF[0] & EL & ~LP & ~ME[1] & ~ME[0] + SA3 & SA2 & SA1 & ME[1] & ME[0]
+ + SA3 & SA2 & ~SA1 & SA0 & ~SSP[1]  + SA3 & SA2 & ~SA1 & SSP[1] & SSP[0] + SA3 & SA2 & ~SA1 & ~SA0 & ~SSP[0];
 
-assign LCD2_0 = SA3 SA2' SA1' SSP1'  + SA3 SA2' SA0' SSP0' + SA3 SA2 SA1 ME1 ME0'  + SA2 SA1 SA0' ME1 ME0'  + SA3 SA2' SA1 SA0' SSP1  + SA3 SA2' SA1' SA0 SSP0 + SA3' SA2' SA0' PF1 EL LP' ME1' ME0'
- + SA3' SA2' SA1' SA0' PF0 EL LP' ME1' ME0'  + SA3' SA1' SA0' PF1 PF0 EL LP' ME1' ME0' ;
+assign LCD2[0] = SA3 & ~SA2 & ~SA1 & ~SSP[1] + SA3 & ~SA2 & ~SA0 & ~SSP[0] + SA3 & SA2 & SA1 & ME[1] & ~ME[0]  + SA2 & SA1 & ~SA0 & ME[1] & ~ME[0] + SA3 & ~SA2 & SA1 & ~SA0 & SSP[1]
++ SA3 & ~SA2 & ~SA1 & SA0 & SSP[0] + ~SA3 & ~SA2 & ~SA0 & PF[1] & EL & ~LP & ~ME[1] & ~ME[0] + ~SA3 & ~SA2 & ~SA1 & ~SA0 & PF[0] & EL & ~LP & ~ME[1] & ~ME[0]
++ ~SA3 & ~SA1 & ~SA0 & PF[1] & PF[0] & EL & ~LP & ~ME[1] & ~ME[0];
 
-assign Mo1 = SA3 SA2' SA1 SA0 SSP1  + SA3 SA2 SA1' SA0' SSP1  + SA3 SA2' SA1 SA0 SSP0 + SA3 SA2 SA1' SSP1 SSP0 + SA3' SA2 SA0' PF1' EL LP' ME1' ME0'
- + SA3' SA2 SA1 SA0' PF0' EL LP' ME1' ME0'  + SA3' SA1 SA0' PF1' PF0' EL LP' ME1' ME0' ;
+assign Mo[1] = SA3 & ~SA2 & SA1 & SA0 & SSP[1]  + SA3 & SA2 & ~SA1 & ~SA0 & SSP[1]  + SA3 & ~SA2 & SA1 & SA0 & SSP[0] + SA3 & SA2 & ~SA1 & SSP[1] & SSP[0]
++ ~SA3 & SA2 & ~SA0 & ~PF[1] & EL & ~LP & ~ME[1] & ~ME[0] + ~SA3 & SA2 & SA1 & ~SA0 & ~PF[0] & EL & ~LP & ~ME[1] & ~ME[0]
++ ~SA3 & SA1 & ~SA0 & ~PF[1] & ~PF[0] & EL & ~LP & ~ME[1] & ~ME[0] ;
 
-assign LED = SA3' SA0 LP  + SA3' LP ME1' ME0' ;
+assign Mo[0] = SA3 & ~SA2 & ~SA1 & ~SSP[1] + SA3 & ~SA2 & ~SA0 & ~SSP[0] + SA3 & ~SA2 & SA1 & ~SA0 & SSP[1]  + SA3 & ~SA2 & ~SA1 & SA0 & SSP[0]
++ ~SA3 & ~SA2 & ~SA0 & PF[1] & EL & ~LP & ~ME[1] & ~ME[0] + ~SA3 & ~SA2 & ~SA1 & ~SA0 & PF[0] & EL & ~LP & ~ME[1] & ~ME[0]
++ ~SA3 & ~SA1 & ~SA0 & PF[1] & PF[0] & EL & ~LP & ~ME[1] & ~ME[0];
 
-assign SN = SA3' SA0 LP  + SA3' LP ME0'  + SA3' SA0' ME1 ME0'  + SA3 SA2 SA1 ME1 ME0' ;
+assign MP = SA3 & ~SA2  + SA3 & ~SA1 + SA3 & ~ME[1] + ~SA0 & ~ME[1] & ME[0]  + ~SA2 & ~SA0 & PF[1] & EL & ~LP & ~ME[1] + SA2 & ~SA0 & ~PF[1] & EL & ~LP & ME[1]
++ ~SA1 & ~SA0 & PF[0] & EL & ~LP & ~ME[1]  + SA1 & ~SA0 & ~PF[0] & EL & ~LP & ~ME[1] ;
 
+assign LED = ~SA3 & SA0 & LP  + ~SA3 & LP & ~ME[1] & ~ME[0];
+
+assign SN = ~SA3 & SA0 & LP  + ~SA3 & LP & ~ME[0] + ~SA3 & ~SA0 & ME[1] & ~ME[0] + SA3 & SA2 & SA1 & ME[1] & ~ME[0];
 
 
 // conectamos los flip flops
@@ -65,3 +74,5 @@ assign SN = SA3' SA0 LP  + SA3' LP ME0'  + SA3' SA0' ME1 ME0'  + SA3 SA2 SA1 ME1
   ffD U1(clk, reset, SF1, SA1);
   ffD U2(clk, reset, SF2, SA2);
   ffD U3(clk, reset, SF3, SA3);
+
+endmodule
